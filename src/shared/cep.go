7b8201d45cf7f96439fb964d7180c43e5659722b
Thiago Tolotti <thiagotolotti@gmail.com>
2025-06-08 01:50:23 -0300

    adds CEP validation and tests

--- src/shared/cep.go
+++ src/shared/cep.go
@@ -1,16 +1,37 @@
 package shared
 
+import (
+	"errors"
+	"regexp"
+)
+
 type CEP struct {
 	value string
 }
 
-// TODO: Add CEP validation
+var (
+	ErrInvalidFormat = errors.New("invalid CEP format")
+
+	sanitizationRegex     = regexp.MustCompile(`[.-]`)
+	digitsValidationRegex = regexp.MustCompile(`^\d{8}$`)
+)
+
 func NewCEP(input string) (CEP, error) {
+	sanitized := sanitizationRegex.ReplaceAllString(input, "")
+
+	if !digitsValidationRegex.MatchString(sanitized) {
+		return CEP{}, ErrInvalidFormat
+	}
+
 	return CEP{
-		value: input,
+		value: sanitized,
 	}, nil
 }
 
-func (cep *CEP) String() string {
+func (cep CEP) String() string {
 	return cep.value
+}
+
+func (cep CEP) Format() string {
+	return cep.value[:2] + "." + cep.value[2:5] + "-" + cep.value[5:]
 }
---
+++ src/shared/cep_test.go
@@ -0,0 +1,67 @@
+package shared_test
+
+import (
+	"testing"
+
+	"comida.app/src/shared"
+)
+
+var validCeps = []string{"80230000", "80.230-000", "80.230000", "80230-000"}
+var invalidCeps = []string{"8023000", "80.230-000a", "80.2300000"}
+
+type CPFValues struct {
+	Input     string
+	Value     string
+	Formatted string
+}
+
+var mockCpf = CPFValues{
+	Input:     "80.230000",
+	Value:     "80230000",
+	Formatted: "80.230-000",
+}
+
+func TestCep(t *testing.T) {
+	t.Run("valid CEPs", func(t *testing.T) {
+		t.Parallel()
+		for _, cep := range validCeps {
+			_, err := shared.NewCEP(cep)
+			if err != nil {
+				t.Errorf("Unexpected error %v", err)
+			}
+		}
+	})
+
+	t.Run("Invalid CEPs", func(t *testing.T) {
+		t.Parallel()
+		for _, cep := range invalidCeps {
+			_, err := shared.NewCEP(cep)
+			if err == nil {
+				t.Errorf("Expected error, got nothing %s", cep)
+			}
+		}
+	})
+
+	t.Run("cpf.String", func(t *testing.T) {
+		t.Parallel()
+		cepObj, err := shared.NewCEP(mockCpf.Input)
+		if err != nil {
+			t.Errorf("Unexpected error %v", err)
+		}
+		if cepObj.String() != mockCpf.Value {
+			t.Errorf("Expected sanitized CEP, got %s", cepObj.String())
+		}
+	})
+
+	t.Run("cpf.Format", func(t *testing.T) {
+		t.Parallel()
+		cepObj, err := shared.NewCEP(mockCpf.Input)
+		if err != nil {
+			t.Fatal("Unexpected Error")
+		}
+
+		if cepObj.Format() != mockCpf.Formatted {
+			t.Errorf("Expected formatted CEP, got %s", cepObj.Format())
+		}
+	})
+}
