19858b018d34ecea2a99f0daec29a4de363a8c1c
Thiago Tolotti <thiago.tolotti@riole.com.br>
2025-08-07 16:00:12 -0300

    adds CreatePedidoRequest struct and validation

--- src/internal/pedido/handler.go
+++ src/internal/pedido/handler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type PedidoHandler struct{}
@@ -16,8 +17,27 @@ func (h *PedidoHandler) RegisterRoutes(router *gin.Engine) {
 	router.POST("/pedido", h.create)
 }
 
+type CreatePedidoRequest struct {
+	Items []CreatePedidoRequestItem `json:"itens" binding:"required,dive,required"`
+}
+
+type CreatePedidoRequestItem struct {
+	ItemId uuid.UUID `json:"item_id" binding:"required"` 
+	Quantity uint32 `json:"quantidade" binding:"required"`
+}
+
+
 func (h *PedidoHandler) create(c *gin.Context) {
+	var body CreatePedidoRequest
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"mensagem": "Pedido inválido",
+		})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Pedido criado com sucesso!",
+		"mensagem": "Pedido criado com sucesso!",
 	} )
 }
