3516c6d360d37fb52d38cc97a42ac3eec729afc4
Thiago Tolotti <thiago.tolotti@riole.com.br>
2025-08-14 16:26:47 -0300

    moves services initialization to ./src/main.go

--- src/cmd/api/main.go
+++ src/cmd/api/main.go
@@ -3,17 +3,16 @@ package api
 import (
 	"net/http"
 
-	"comida.app/src/adapters"
-	"comida.app/src/infra/eventbus"
-	"comida.app/src/internal/notificacoes"
-	"comida.app/src/internal/pedido"
-	"comida.app/src/internal/restaurante"
 	"github.com/gin-gonic/gin"
 )
 
 const PORT = "3001"
 
-func Start() {
+type Handlers interface {
+	RegisterRoutes(r *gin.Engine)
+}
+
+func Start(handlers []Handlers) {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
@@ -22,46 +21,10 @@ func Start() {
 		})
 	})
 
-	eventBus := eventbus.NewEventBus()
-	
-	notificacoes.NewNotificacoesService(eventBus)
-
-	restauranteRepo := restaurante.NewInMemoryRestauranteRepository()
-	restauranteService := restaurante.NewRestauranteService(restauranteRepo)
-	restauranteHandler := restaurante.NewRestaurantHandler(*restauranteService)
-	restauranteHandler.RegisterRoutes(router)
-
-	pedidoRepo := pedido.NewInMemoryPedidoRepository()
-	pedidoService := pedido.NewPedidoService(pedidoRepo, adapters.NewCardapioPedidoAdapter(restauranteService), eventBus)
-	pedidoHandler := pedido.NewPedidoHandler(*pedidoService)
-	pedidoHandler.RegisterRoutes(router)
-
-	initializeRestaurante(restauranteService)
+	for _, handler := range handlers {
+		handler.RegisterRoutes(router)
+	}
 
 	router.Run(":" + PORT)
 }
 
-func initializeRestaurante(s *restaurante.RestauranteService) {
-	cnpj, err := restaurante.NewCNPJ("18781203/0001-28")
-	if err != nil {
-		panic(err)
-	}
-
-	id := s.Create(cnpj, "Santo Crepe")
-	itemID, err := s.CreateMenuItem(id, restaurante.MenuItemParams{
-		Name:     "Item de teste",
-		Price:    420,
-		Category: "teste",
-	})
-
-	if err != nil {
-		panic(err)
-	}
-
-	s.UpdateMenuItem(id, itemID, restaurante.MenuItemParams{
-		Name:     "Item de teste atualizado",
-		Price:    421,
-		Category: "teste",
-	})
-
-}
