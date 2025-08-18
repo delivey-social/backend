b2d5880aece846bb217e560b491eef4c172ae6b1
Thiago Tolotti <thiagotolotti@gmail.com>
2025-08-17 22:37:01 -0300

    changes address related vos from shared to pedido contexto

--- src/internal/pedido/vo_endereco_test.go
+++ src/internal/pedido/vo_endereco_test.go
@@ -1,25 +1,23 @@
-package shared_test
+package pedido_test
 
 import (
 	"testing"
 
-	"comida.app/src/shared"
+	"comida.app/src/internal/pedido"
 )
 
 func TestEndereco(t *testing.T) {
-	validCEP, _ := shared.NewCEP("80000000")
+	validCEP, _ := pedido.NewCEP("80000000")
 	validRua := "Rua Teste"
 	validBairro := "Bairro Teste"
 	validNumero := "100"
 	validComplemento := "Apto 1"
-	validCidade := "Cidade Teste"
-	validUF := "SP"
-	validCoordinate, _ := shared.NewCoordenada(50.44, 40.44)
+	// validCidade := "Cidade Teste"
+	// validUF := "SP"
 
 	t.Run("It should create an endereço in all valid values", func(t *testing.T) {
-		endereco, err := shared.NewEndereco(
+		endereco, err := pedido.NewEndereco(
 			validCEP, validRua, validBairro, validNumero, validComplemento,
-			validCidade, validUF, validCoordinate,
 		)
 		if err != nil {
 			t.Fatalf("expected no error, got %v", err)
@@ -39,23 +37,14 @@ func TestEndereco(t *testing.T) {
 		if endereco.Complemento != validComplemento {
 			t.Errorf("expected Complemento %v, got %v", validComplemento, endereco.Complemento)
 		}
-		if endereco.Cidade != validCidade {
-			t.Errorf("expected Cidade %v, got %v", validCidade, endereco.Cidade)
-		}
-		if endereco.UF != validUF {
-			t.Errorf("expected UF %v, got %v", validUF, endereco.UF)
-		}
-		if endereco.Coordenada != validCoordinate {
-			t.Errorf("expected Coordenada %v, got %v", validCoordinate, endereco.Coordenada)
-		}
+
 	})
 
 	t.Run("It should return an error in invalid values", func(t *testing.T) {
-		_, err := shared.NewEndereco(
+		_, err := pedido.NewEndereco(
 			validCEP, "", validBairro, validNumero, validComplemento,
-			validCidade, validUF, validCoordinate,
 		)
-		if err != shared.ErrInvalidField {
+		if err != pedido.ErrInvalidField {
 			t.Errorf("expected ErrInvalidField, got %v", err)
 		}
 	})
