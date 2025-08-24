894567f36a726dec5a6cd9d2c4fd0085d5906891
Thiago Tolotti <thiagotolotti@gmail.com>
2025-08-24 10:45:24 -0300

    adds total price formatted

--- src/internal/pedido/handler_create.go
+++ src/internal/pedido/handler_create.go
@@ -58,7 +58,9 @@ func (h *PedidoHandler) create(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"mensagem": "Pedido criado com sucesso!",
 		"id":       id,
-		"preco":    pedido.CalculateTotal(),
+		"preco": gin.H{
+			"total_itens": pedido.CalculateTotal(),
+		},
 	})
 }
 
